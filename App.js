c57372ed577500d4ced7426a533cd03b4cd6c7d2
Nadine Ju <nadine.jura@online.de>
2020-05-22 15:03:08 -0700

    basic search screen created

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import SearchScreen from './src/screens/SearchScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createStackNavigator({
+  Search: SearchScreen
+}, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'Business Search'
+  }
 });
+
+export default createAppContainer(navigator);
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const SearchScreen = () => {
+    return (
+        <View>
+            <Text>Search Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default SearchScreen;
